e7ec439a23f5a17bb4708dcd2efa08365efb5587
Simon Larsson <simon.larsson90@hotmail.com>
2019-11-12 16:02:04 +0100

    Changed to Date.now

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,43 +1,77 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css';
 
-
 function Timer() {
+  const [time, setTime] = useState(0)
+  const [start, setStart] = useState(0)
   const [status, setStatus] = useState(false)
-  const [timer, setTimer] = useState(0)
+  const [splits, setSplits] = useState([])
 
   
-  // Varje gång status ändras körs den här useEffect
+
+  const startTimer = () => {
+    setTime(time)
+    setStart(Date.now() - time)
+    console.log(time)
+    setStatus(true)
+  }
+
+  const stopTimer = () => {
+    setStatus(false)
+  }
+
+  const resetInterval = () => {
+    setTime(0)
+    setSplits([])
+  }
+
+  const createLap = () => {
+    setSplits([...splits, time])
+  }
+
+
+
+
+  //Varje gång status ändras körs den här useEffect
   useEffect(() => {
     if (status === true) {
-      // från useRef använder vi .current
-      const intervalId = setInterval(() => {
 
-        // Lägger på 10 till förra värdet (körs varje millesekund)
-        setTimer(prev => prev + 10)
-      }, 10)
-
+      const intervalId = setInterval(() => setTime(Date.now() - start), 10)
+      
       // Avslutar intervalen
-    return () => clearInterval(intervalId)
+      return () => clearInterval(intervalId)
     }
-  }, [status])
+  })
 
 
 
-  let centiseconds = ("0" + (Math.floor(timer / 10) % 100)).slice(-2);
-  let seconds = ("0" + (Math.floor(timer / 1000) % 60)).slice(-2);
-  let minutes = ("0" + (Math.floor(timer / 60000) % 60)).slice(-2);
-  let hours = ("0" + Math.floor(timer / 3600000)).slice(-2);
-
-
+  function formatTime(ms) {
+    let min = Math.floor((ms / (1000 * 60)) % 60);
+    let sec = ((ms % 60000) / 1000).toFixed(2);
+    return `${min < 10 ? '0' + min : min}.${sec < 10 ? '0' + sec : sec}`;
+  }
 
   return (
     <div className="App">
-      <div className="Timer">{hours} : {minutes} : {seconds}</div>
+      <div className="Timer">{formatTime(time)}</div>
       <div className="Btn">
-      <button onClick={() => { setStatus(true) }}>start</button>
-      <button onClick={() => { setStatus(false) }}>stop</button>
-      <button onClick={() => { setTimer(0) }}>reset</button>
+
+        <button onClick={() => { startTimer() }}>start</button>
+        <button onClick={() => { stopTimer() }}>stop</button>
+        <button onClick={() => { createLap() }}>laps</button>
+        <button onClick={() => { resetInterval() }}>reset</button>
+        <ul>
+          {
+            splits.map((l, id) => {
+              console.log(id)
+              return (
+                <li key={id}>
+                  <p>LAP {id + 1}</p> + {formatTime(l)}
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     </div>
   );
